c2eb338d8764bc093a53aa1477962f22844e84a3
William Ribeiro <william-cribeiro@bct.ect.ufrn.br>
2018-08-21 17:10:46 -0300

    add Somador

---
+++ Somador4bits/somador1bit.vhd
@@ -0,0 +1,15 @@
+entity soma1bit is
+ port( A,B,Ci: in bit;
+	Co,S: out bit);
+end soma1bit;
+
+architecture ckt of soma1bit is
+
+	begin 
+
+	Co <= (A and B) or (A and Ci) or (B and Ci);
+	S <= A xor B xor Ci;
+	
+	
+
+	end ckt;
